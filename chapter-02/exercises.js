abbd32f521174d4f7e221323c6cf1f5cf33f6671
sandrockjustin <sandrock.justin@gmail.com>
2024-05-30 20:26:14 -0500

    + most of drawChessboard is complete

--- chapter-02/exercises.js
+++ chapter-02/exercises.js
@@ -94,7 +94,11 @@ will be a collection of spaces and #'s creating the appearance of a chessboard.
 
 Note: in order to do this correctly, you need to ultimately create a string containing
 linebreak characters (\n). For example, if you invoke drawChessboard(3) it should log a
-string that looks like this " # \n# #\n # \n# #"
+string that looks like this 
+" # \n
+# #\n
+ # \n
+# #"
 
 example: drawChessboard(4);
 LOGS =>
@@ -114,10 +118,56 @@ LOGS =>
 */
 
 function drawChessboard(x) {
+  
+  function originalBoard(x){
+    let localString = [];
+    for (let i = 0; i < x; i++){
+      
+      if (i % 2 == 0){
+        localString += " ";
+      } else {
+        localString += "#";
+      }
+      
+    }
+    return localString;
+  }
 
-
+  function alternateBoard(x){
+    let localString = [];
+    for (let i = 0; i < x; i++){
+      
+      if (i % 2 == 0){
+        localString += "#";
+      } else {
+        localString += " ";
+      }
+      
+    }
+    return localString;
+  }
+  
+  // a localString needs to be declared at the top of drawChessboard
+  // the localString will need to receive += assignments
+  // conditionals will dictate whether a \n is added
+  
+  for (let y = 0; y < x; y++){
+    
+    if (y % 2 == 0){
+      
+      console.log(originalBoard(x)); // these will have to be changed to assignment operators
+      
+    } else {
+      
+      console.log(alternateBoard(x)); // these will have to be changed to assignment operators
+      
+    }  
+    
+  }
+  
 }
 
+console.log(drawChessboard(10));
 ////////////////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
