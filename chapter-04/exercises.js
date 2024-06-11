cdbc8fd1bddd7138b8674d1b8ad8f77aaf378414
sandrockjustin <sandrock.justin@gmail.com>
2024-06-11 14:22:19 -0500

    completed range

--- chapter-04/exercises.js
+++ chapter-04/exercises.js
@@ -2,7 +2,48 @@
 // range ///////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function range() {
+function range(x, y, step = 1) {
+  /*
+  // if (x, y) arguments are the same number, or if there is no whole number between them
+  if (x === y || x + 1 === y || y - 1 === x){
+    return [];
+  }
+
+  let output = [];
+
+  
+  // if the starting number (x) is less than the ending number (y)
+  if (x < y){
+
+    // for each number between (x) and (y), push to the output
+    for (x += 1; x < y; x++){
+      output.push(x);
+    }
+
+  // if the starting number (x) is greater than the ending number (y)
+  } else if (x > y){
+
+    // for each number between (x) and (y), push to the output
+    for (x -= 1; x > y; x--){
+      output.push(x);
+    }
+
+  }
+  */
+
+  let output = [];
+
+  // if (x, y) arguments are the same number, or if there is no whole number between them
+  if (x === y || step < 0){
+    return [];
+  }
+
+  for (x; x <= y; x += step){
+    output.push(x);
+  }
+
+  // once tasks are complete, return the output
+  return output;
 
 }
 
